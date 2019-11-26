935251deb412d554f905942a41a6b9554500955b
spg <s.gingras@botpress.io>
2019-11-26 11:18:18 -0800

    Copy and paste from example

--- BotpressUiPath/Class1.cs
+++ BotpressUiPath/Class1.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
 
 namespace Botpress
 {
-    public class Class1
+    public class SimpleFormula : CodeActivity
     {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> ResultNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var result = System.Math.Pow(firstNumber + secondNumber, 2);
+            ResultNumber.Set(context, result);
+        }
     }
 }
